1ea50b99909fda15eb0171e0e5c43f61a50ed877
Sarah Jelimo <ksarahjelimo@gmail.com>
2022-03-22 10:19:43 +0300

    Made slight changes on button functionality

--- main.js
+++ main.js
@@ -4,4 +4,9 @@ function Pizza(size, crust,topping ){
     this.topping = topping;
 }
 
-var newOrder = new Pizza($("[name = gender]").val();)
+var newOrder = new Pizza(document.getElementById("size").value,document.getElementById("crust").value,document.getElementById("topping").value )
+
+function priceOfOrder(){
+    var price = newOrder.size + newOrder.crust + newOrder.topping;
+    return price;
+}
